db22cd2bf2edd635b7cd3dc1f8be0ef3a4f573bb
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-01 14:03:38 -0600

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -77,6 +77,18 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       }
       chat.antiLink = isEnable
       break
+    case 'nsfw':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          global.dfail('group', m, conn)
+          throw false
+        }
+      } else if (!isAdmin) {
+        global.dfail('admin', m, conn)
+        throw false
+      }
+      chat.nsfw = isEnable
+      break
     case 'toxic':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {
